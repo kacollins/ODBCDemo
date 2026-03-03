79a919c2bb205549395029216c1d98641450d0d4
Kimberly Collins <kimberlyacollins@gmail.com>
2026-03-03 09:34:15 -0600

    Add join, view, and function examples

--- ODBCDemo/Program.cs
+++ ODBCDemo/Program.cs
@@ -18,16 +18,13 @@ class Program
             connection.Open();
             Console.WriteLine("Connected to Pagila via ODBC!");
 
-            GetActors(connection);
+            string lastName = "Collins";
+            GetActors(connection, lastName);
+            GetFilmsByActorLastName(connection, lastName);
 
-            InsertActor(connection, "Kimberly", "Collins");
-            GetActors(connection, "Collins");
+            GetSalesByFilmCategory(connection);
 
-            UpdateActor(connection, "Kimberly", "Collins", "NewLastName");
-            GetActors(connection, "NewLastName");
-
-            //DeleteActor(connection, "Kimberly", "NewLastName");
-            //GetActors(connection, "NewLastName");
+            GetLastDayOfCurrentMonth(connection);
         }
         catch (OdbcException ex)
         {
@@ -121,6 +118,71 @@ class Program
 
         int rows = command.ExecuteNonQuery();
         Console.WriteLine($"Deleted {rows} row(s)");
+        Console.WriteLine();
+    }
+
+    private static void GetFilmsByActorLastName(OdbcConnection connection, string lastName)
+    {
+        string sql = @"SELECT a.first_name, a.last_name, f.title
+                        FROM actor a
+                        JOIN film_actor fa ON a.actor_id = fa.actor_id
+                        JOIN film f ON fa.film_id = f.film_id
+                        WHERE last_name = ?"; //example of joins
+
+        using var command = new OdbcCommand(sql, connection);
+        command.Parameters.AddWithValue("@last", lastName);
+
+        using var reader = command.ExecuteReader();
+
+        Console.WriteLine($"Films for actors with last name {lastName}:");
+
+        while (reader.Read())
+        {
+            string first = reader.GetString(reader.GetOrdinal("first_name"));
+            string last = reader.GetString(reader.GetOrdinal("last_name"));
+            string title = reader.GetString(reader.GetOrdinal("title"));
+            Console.WriteLine($"{first} {last} was in {title}");
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void GetSalesByFilmCategory(OdbcConnection connection)
+    {
+        string sql = @"SELECT category, total_sales
+	                    FROM public.sales_by_film_category"; //example of a view
+
+        using var command = new OdbcCommand(sql, connection);
+
+        using var reader = command.ExecuteReader();
+
+        Console.WriteLine($"Sales by Film Category:");
+
+        while (reader.Read())
+        {
+            string category = reader.GetString(reader.GetOrdinal("category"));
+            decimal total_sales = reader.GetDecimal(reader.GetOrdinal("total_sales"));
+            Console.WriteLine($"{category} {total_sales:C2}");
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void GetLastDayOfCurrentMonth(OdbcConnection connection)
+    {
+        string sql = @"SELECT public.last_day(?)"; //example of a function
+
+        using var command = new OdbcCommand(sql, connection);
+        command.Parameters.AddWithValue("@timestamp", DateTime.Now.ToString());
+
+        using var reader = command.ExecuteReader();
+
+        if (reader.Read())
+        {
+            var date = reader.GetDate(0);
+            Console.WriteLine($"Last Day of Current Month: {date:d}");
+        }
+
         Console.WriteLine();
     }
 }
